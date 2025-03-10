bf096191a05556ea0e709d0e0d34308a36b6fd39
Pablo-Henrique-IA <pablohmsilva7@gmail.com>
2025-03-10 20:04:59 -0300

    Update func-slice.py

--- func-slice.py
+++ func-slice.py
@@ -1,3 +1,12 @@
 reverso = lambda s: s[::-1]
-print(reverso("Python")) # nohtyP
+print(reverso("\n Python \n")) # nohtyP
 print(reverso([1, 2, 3])) # [3, 2, 1]
+
+nome = "Python"
+print(len(nome)) # 6
+
+cincoPrimeiros = lambda s: s[:5]
+print(cincoPrimeiros(nome)) # Pytho
+
+cincoPrimeiros_2 = lambda s: s[1:6]
+print(cincoPrimeiros_2(nome)) # ython
